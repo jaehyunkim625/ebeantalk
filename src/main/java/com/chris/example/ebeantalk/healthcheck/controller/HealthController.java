2ac33842daad9637353631bbf1bc0e4db13f3481
Chris Kim <jaehyunkim625@gmail.com>
2021-01-18 10:16:41 +0900

    mod hello msg

--- src/main/java/com/chris/example/ebeantalk/healthcheck/controller/HealthController.java
+++ src/main/java/com/chris/example/ebeantalk/healthcheck/controller/HealthController.java
@@ -8,7 +8,7 @@ public class HealthController {
 
     @GetMapping("/")
     public String healthCheck(){
-        return "ok";
+        return "Hi, BeansTalk!";
     }
 
     @GetMapping("/sayHello")
